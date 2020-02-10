63f77010f7edce5ca6efb98a5b199307fd7f0f6e
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2020-02-10 02:52:47 -0500

    Update my model

--- ps1/hw1_q8_starter/assignment/2_pytorch/models/mymodel.py
+++ ps1/hw1_q8_starter/assignment/2_pytorch/models/mymodel.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 class MyModel(nn.Module):
     def __init__(self, im_size, hidden_dim, kernel_size, n_classes):
         '''
-        Extra credit model
+        Create components of a CNN classifier and initialize their weights.
 
         Arguments:
             im_size (tuple): A tuple of ints with (channels, height, width)
@@ -19,14 +19,22 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Initialize anything you need for the forward pass
         #############################################################################
-        pass
+        self.pool_size = 2
+        self.conv_relu_conv_relu_pool = nn.Sequential(
+            nn.Conv2d(im_size[0], hidden_dim, kernel_size=kernel_size, stride=1, padding=(kernel_size - 1) // 2),
+            nn.ReLU(),
+            nn.Conv2d(im_size[0], hidden_dim, kernel_size=kernel_size, stride=1, padding=(kernel_size - 1) // 2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=self.pool_size)
+        )
+        self.fully_connected = nn.Linear(hidden_dim * (im_size[1] // self.pool_size) * (im_size[2] // self.pool_size), n_classes)
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
 
     def forward(self, images):
         '''
-        Take a batch of images and run them through the model to
+        Take a batch of images and run them through the CNN to
         produce a score for each class.
 
         Arguments:
@@ -42,9 +50,10 @@ class MyModel(nn.Module):
         '''
         scores = None
         #############################################################################
-        # TODO: Implement the forward pass.
+        # TODO: Implement the forward pass. This should take few lines of code.
         #############################################################################
-        pass
+        out = self.conv_relu_conv_relu_pool(images)
+        scores = self.fully_connected(out.view(images.shape[0], -1))
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
