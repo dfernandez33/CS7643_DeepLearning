4d7e8eaeffc1c51f302ad510b0eb881948fddf22
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2020-02-10 04:48:35 -0500

    Update my model

--- ps1/hw1_q8_starter/assignment/2_pytorch/models/mymodel.py
+++ ps1/hw1_q8_starter/assignment/2_pytorch/models/mymodel.py
@@ -64,7 +64,6 @@ class MyModel(nn.Module):
         out = None
         num_convs = 2
         for i in range(num_convs):
-            print(i)
             if i == 0:
                 out = self.conv_relu_conv_relu_pool_1(images)
             else:
